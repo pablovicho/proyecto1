0e846cc4e3149e1ff46aecd9c766c40e9e48ccb1
pablovicho <pablovvb@gmail.com>
2021-11-10 12:01:17 -0600

    cleaned the code. dice is showing but not the text

--- script.js
+++ script.js
@@ -27,29 +27,6 @@ diccionario.readFile('diccionario.txt', (err, data) => {
 
 })*/
 
-//dados
-const $dado1 = document.querySelector("dado1");
-/*const $dado2 = document.querySelector("dado2");
-const $dado3 = document.querySelector("dado3");
-const $dado4 = document.querySelector("dado4");
-
-const $dado5 = document.querySelector("dado5");
-const $dado6 = document.querySelector("dado6");
-const $dado7 = document.querySelector("dado7");
-const $dado8 = document.querySelector("dado8");
-
-const $dado9 = document.querySelector("dado9");
-const $dado10 = document.querySelector("dado10");
-const $dado11 = document.querySelector("dado11");
-const $dado12 = document.querySelector("dado12");
-
-const $dado13 = document.querySelector("dado13");
-const $dado14 = document.querySelector("dado14");
-const $dado15 = document.querySelector("dado15");
-const $dado16 = document.querySelector("dado16");*/
-
-
-
 //clases
 class Board {
     constructor() {
@@ -66,19 +43,23 @@ class Board {
 }
 
 class Dado {
-    constructor(array, imageurl, element) {
-        this.array = array;
-        this.element = element;
-        this.image = new Image();
-        this.imageurl = imageurl; 
+    constructor(x, y, letra) {
+        this.x = x;
+        this.y = y;
+        this.img = new Image();
+        this.img.src = "/imagenes/empty-dice.png";
+        this.letra = letra;
     }
 
-    draw(source) {
-        if(source) {
-        ctx.drawImage(source, 10, 10, 10, 10);
-    } else {(source = "/imagenes/empty-dice.png")}
-    console.log("llama el código a dado.draw?")
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, 150, 150);
+        ctx.font = '50px serif';
+        if(this.letra) {
+        ctx.fillText(this.letra);
+        } else {
+        ctx.fillText("A", this.x, this.y);
     }
+}
 
     revolver(array) {
         let random = Math.floor(Math.random() * array.length);
@@ -93,61 +74,11 @@ class Dado {
 }
 
 //instancias
-
-/*
-const dado2 = new Dado(['e','l','r','t','t','y'], "/imagenes/empty-dice.png", $dado2);
-const dado3 = new Dado(['a','o','o','t','t','ñ'], "/imagenes/empty-dice.png", $dado3);
-const dado4 = new Dado(['a','b','b','j','o','o'], "/imagenes/empty-dice.png", $dado4);
-
-const dado5 = new Dado(['e','h','r','t','v','w'], "/imagenes/empty-dice.png", $dado5);
-const dado6 = new Dado(['c','i','m','o','t','u'], "/imagenes/b.png", $dado6);
-const dado7 = new Dado(['d','i','s','t','t','y'], "/imagenes/o.png", $dado7);
-const dado8 = new Dado(['e','i','o','s','s','t'], "/imagenes/g.png", $dado8);
-
-const dado9 = new Dado(['d','e','l','r','v','y'], "/imagenes/g.png", $dado9);
-const dado10 = new Dado(['a','c','h','o','p','s'], "/imagenes/l.png", $dado10);
-const dado11 = new Dado(['h','i','m','n','u','q'], "/imagenes/e.png", $dado11);
-const dado12 = new Dado(['e','e','i','n','s','u'], "/imagenes/empty-dice.png", $dado12);
-
-const dado13 = new Dado(['e','e','g','h','n','ñ'], "/imagenes/empty-dice.png", $dado13);
-const dado14 = new Dado(['a','f','f','k','p','s'], "/imagenes/empty-dice.png", $dado14);
-const dado15 = new Dado(['h','l','n','n','r','z'], "/imagenes/empty-dice.png", $dado15);
-const dado16 = new Dado(['d','e','i','l','r','x'], "/imagenes/empty-dice.png", $dado16);
-*/
-const dados = [[dado1, dado2, dado3, dado4], // AAEEGN, ELRTTY, AOOTTW, ABBJOO, 
-[dado5,dado6, dado7, dado8], // EHRTVW, CIMOTU, DISTTY, EIOSST, 
-[dado9, dado10, dado11, dado12], // DELRVY, ACHOPS, HIMNQuU, EEINSU, 
-[dado13, dado14, dado15, dado16, ]]; // EEGHNW, AFFKPS, HLNNRZ, DEILRX
-
-//no sé si sea mejor en objeto, en array de arrays, o una clase, 
-//pero sí es importante que no sean completamente aleatorios, para que haya más vocales que consonantes
-//me preocupa que se repitan tanto estas variables. no me parece óptimo
-
-
-
+let dado1 = new Dado(0, 0);
+let board = new Board();
 
 function revolverDados() {
-   /*
-    $dado1 = dado1.revolver(dados[0]);
-    $dado2 = dado2.revolver(dados[1]);
-    $dado3 = dado3.revolver(dados[2]);
-    $dado4 = dado4.revolver(dados[3]);
 
-    $dado5 = dado5.revolver(dados[4]);
-    $dado6 = dado6.revolver(dados[5]);
-    $dado7 = dado7.revolver(dados[6]);
-    $dado8 = dado8.revolver(dados[7]);
-    
-    $dado9 = dado9.revolver(dados[8]);
-    $dado10 = dado10.revolver(dados[9]);
-    $dado11 = dado11.revolver(dados[10]);
-    $dado12 = dado12.revolver(dados[11]);
-    
-    $dado13 = dado13.revolver(dados[12]);
-    $dado14 = dado14.revolver(dados[13]);
-    $dado15 = dado15.revolver(dados[14]);
-    $dado16 = dado16.revolver(dados[15]);
-    */
 //esto debería realizar la función revolver para todos los dados
 //sin embargo, es repetitivo, y debería poder usar la clase dado para crearlo
 //también debería ser intuitivo el usar la imagen que tenga asignada
@@ -205,15 +136,12 @@ function start() {
 }
 
 
-    // 	
-    // 	
-    // };
-
-
 function update() {
     reloj--;
     console.log(reloj);
     if (reloj === 0) endGame();
+    board.draw();
+    dado1.draw();
 }
 
 function endGame() {
@@ -228,10 +156,7 @@ function endGame() {
 
 
 window.onload = (event) => {
-    let board = new Board();
-    board.draw();
-    let dado1 = new Dado($dado1);
-    dado1.draw($dado1);
+    dado1.draw(0, 0, "a");
 };
 
 document.getElementById('start').onclick = () => {
@@ -239,3 +164,44 @@ document.getElementById('start').onclick = () => {
     start();
     intervalId = setInterval(update, 1000);
 }
+
+
+
+
+
+
+
+
+
+
+/*
+['e','l','r','t','t','y']
+['a','o','o','t','t','ñ']
+['a','b','b','j','o','o']
+
+['e','h','r','t','v','w']
+['c','i','m','o','t','u']
+['d','i','s','t','t','y']
+['e','i','o','s','s','t']
+
+['d','e','l','r','v','y']
+['a','c','h','o','p','s']
+['h','i','m','n','u','q']
+['e','e','i','n','s','u']
+
+['e','e','g','h','n','ñ']
+['a','f','f','k','p','s']
+['h','l','n','n','r','z']
+['d','e','i','l','r','x']
+*/
+
+
+/*const dados = [[dado1, dado2, dado3, dado4], // AAEEGN, ELRTTY, AOOTTW, ABBJOO, 
+[dado5,dado6, dado7, dado8], // EHRTVW, CIMOTU, DISTTY, EIOSST, 
+[dado9, dado10, dado11, dado12], // DELRVY, ACHOPS, HIMNQuU, EEINSU, 
+[dado13, dado14, dado15, dado16, ]]; // EEGHNW, AFFKPS, HLNNRZ, DEILRX*/
+
+
+//no sé si sea mejor en objeto, en array de arrays, o una clase, 
+//pero sí es importante que no sean completamente aleatorios, para que haya más vocales que consonantes
+//me preocupa que se repitan tanto estas variables. no me parece óptimo
