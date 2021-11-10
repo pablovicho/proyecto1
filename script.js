6827f62597ead9f655acd59c0d8ff866e241e723
pablovicho <pablovvb@gmail.com>
2021-11-10 15:20:03 -0600

    re-coded the draw function, now the board uses a random word as supposed

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //---------------------CONTADORES
+//@import url('')
 let score = 0;
 let lista = [];
 let intervalId;
 let reloj = 6;
+let array = ['e','l','r','t','t','y','a','o','o','t','t','ñ','a','b','b','j','o','o','e','h','r','t','v','w','c','i','m','o','t','u','d','i','s','t','t','y','e','i','o','s','s','t','d','e','l','r','v','y','a','c','h','o','p','s','h','i','m','n','u','q','e','e','i','n','s','u','e','e','g','h','n','ñ','a','f','f','k','p','s','h','l','n','n','r','z','d','e','i','l','r','x']
+
 
 //------------------VARIALES GLOBALES
 const $start = document.querySelector("start");
@@ -38,20 +41,13 @@ class Dado {
 
     draw() {
         ctx.drawImage(this.img, this.x, this.y, 150, 150);
-        ctx.font = '50px serif';
+        ctx.font = 'bold 50px Indie-Flower-Regular';
         if(this.letra) {
-        ctx.fillText(this.xLetra, this.yLetra, this.letra);
+        ctx.fillText(`${this.letra}`, this.xLetra, this.yLetra);
         } else {
-        ctx.fillText("A", this.xLetra, this.yLetra);
+        ctx.fillText("", this.xLetra, this.yLetra);
     }
 }
-
-    revolver(array) {
-        let random = Math.floor(Math.random() * array.length);
-        return array[random];
-        //esto regresa un número del 0 al 5, que se va a usar para elegir una letra de cada dado
-        //sin embargo, necesito asignar a cada una de estas letras una respectiva imagen
-        }
 
    // newImage(element,imageurl) {
      //       element.style.backgroundImage = "url("+imageurl+")";
@@ -61,23 +57,30 @@ class Dado {
 
 //-------------------------INSTANCIAS
 let board = new Board();
-let dado1 = new Dado(0, 0, xLetra(1), yLetra(1), "");
-let dado2 = new Dado(150, 0, xLetra(2), yLetra(1), "B");
-let dado3 = new Dado(300, 0, xLetra(3), yLetra(1), "O");
-let dado4 = new Dado(450, 0, xLetra(4), yLetra(1), "G");
-let dado5 = new Dado(0, 150, xLetra(1), yLetra(2), "G");
-let dado6 = new Dado(150, 150, xLetra(2), yLetra(2), "L");
-let dado7 = new Dado(300, 150, xLetra(3), yLetra(2), "E");
-let dado8 = new Dado(450, 150, xLetra(4), yLetra(2), "");
-let dado9 = new Dado(0, 300, xLetra(1), yLetra(3), "");
-let dado10 = new Dado(150, 300, xLetra(2), yLetra(3), "");
-let dado11 = new Dado(300, 300, xLetra(3), yLetra(3), "");
-let dado12 = new Dado(450, 300, xLetra(4), yLetra(3), "");
-let dado13 = new Dado(0, 450, xLetra(1), yLetra(4), "");
-let dado14 = new Dado(150, 450, xLetra(2), yLetra(4), "");
-let dado15 = new Dado(300, 450, xLetra(3), yLetra(4), "");
-let dado16 = new Dado(450, 450, xLetra(4), yLetra(4), "");
+let dado1 = new Dado(0, 0, xLetra(1), yLetra(1), `${revolver(array).toUpperCase()}`);
+let dado2 = new Dado(150, 0, xLetra(2), yLetra(1), `${revolver(array).toUpperCase()}`);
+let dado3 = new Dado(300, 0, xLetra(3), yLetra(1), `${revolver(array).toUpperCase()}`);
+let dado4 = new Dado(450, 0, xLetra(4), yLetra(1), `${revolver(array).toUpperCase()}`);
+let dado5 = new Dado(0, 150, xLetra(1), yLetra(2), `${revolver(array).toUpperCase()}`);
+let dado6 = new Dado(150, 150, xLetra(2), yLetra(2), `${revolver(array).toUpperCase()}`);
+let dado7 = new Dado(300, 150, xLetra(3), yLetra(2), `${revolver(array).toUpperCase()}`);
+let dado8 = new Dado(450, 150, xLetra(4), yLetra(2), `${revolver(array).toUpperCase()}`);
+let dado9 = new Dado(0, 300, xLetra(1), yLetra(3), `${revolver(array).toUpperCase()}`);
+let dado10 = new Dado(150, 300, xLetra(2), yLetra(3), `${revolver(array).toUpperCase()}`);
+let dado11 = new Dado(300, 300, xLetra(3), yLetra(3), `${revolver(array).toUpperCase()}`);
+let dado12 = new Dado(450, 300, xLetra(4), yLetra(3), `${revolver(array).toUpperCase()}`);
+let dado13 = new Dado(0, 450, xLetra(1), yLetra(4), `${revolver(array).toUpperCase()}`);
+let dado14 = new Dado(150, 450, xLetra(2), yLetra(4), `${revolver(array).toUpperCase()}`);
+let dado15 = new Dado(300, 450, xLetra(3), yLetra(4), `${revolver(array).toUpperCase()}`);
+let dado16 = new Dado(450, 450, xLetra(4), yLetra(4), `${revolver(array).toUpperCase()}`);
 //-------------------------FUNCIONES
+
+function revolver(array) {
+    let random = Math.floor(Math.random() * array.length);
+    return array[random];
+    //esto regresa un número del 0 al 5, que se va a usar para elegir una letra de cada dado
+    //sin embargo, necesito asignar a cada una de estas letras una respectiva imagen
+    }
 
 function xLetra(num) {
     return (600/4)*num-(600/8)-18;
@@ -139,8 +142,23 @@ function start() {
 	// 3. Dibujar los elementos
     let board = new Board();
     board.draw();
-	let dado1 = new Dado();
-    dado1.draw();
+dado1.draw();
+dado2.draw();
+dado3.draw();
+dado4.draw();
+dado5.draw();
+dado6.draw();
+dado7.draw();
+dado8.draw();
+dado9.draw();
+dado10.draw();
+dado11.draw();
+dado12.draw();
+dado13.draw();
+dado14.draw();
+dado15.draw();
+dado16.draw();
+
 
   //checkCollitions();
 }
@@ -151,7 +169,7 @@ function update() {
     console.log(reloj);
     if (reloj === 0) endGame();
     board.draw();
-    dado1.draw();
+
 }
 
 function endGame() {
@@ -162,26 +180,15 @@ function endGame() {
     reloj = 6;
     console.log(reloj);
     intervalId = null;
+    reStart();
 }
 
+function reStart() {
+//esto debería permitir que se pueda volver a hacer click en start
+}
 
 window.onload = (event) => {
-    dado1.draw();
-    dado2.draw();
-    dado3.draw();
-    dado4.draw();
-    dado5.draw();
-    dado6.draw();
-    dado7.draw();
-    dado8.draw();
-    dado9.draw();
-    dado10.draw();
-    dado11.draw();
-    dado12.draw();
-    dado13.draw();
-    dado14.draw();
-    dado15.draw();
-    dado16.draw();
+    reStart();
 };
 
 document.getElementById('start').onclick = () => {
@@ -220,24 +227,7 @@ esto me permite quitar acentos
 
 
 /*
-['e','l','r','t','t','y']
-['a','o','o','t','t','ñ']
-['a','b','b','j','o','o']
 
-['e','h','r','t','v','w']
-['c','i','m','o','t','u']
-['d','i','s','t','t','y']
-['e','i','o','s','s','t']
-
-['d','e','l','r','v','y']
-['a','c','h','o','p','s']
-['h','i','m','n','u','q']
-['e','e','i','n','s','u']
-
-['e','e','g','h','n','ñ']
-['a','f','f','k','p','s']
-['h','l','n','n','r','z']
-['d','e','i','l','r','x']
 */
 
 
