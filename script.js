07eaa87d33d9e1423bf3d31f14f39cc28233cf05
pablovicho <pablovvb@gmail.com>
2021-11-10 23:50:56 -0600

    trying to implement mouse event listeners

--- script.js
+++ script.js
@@ -1,19 +1,88 @@
-//---------------------CONTADORES
+//---------------------CONTADORES Y VARIABLES GLOBALES
 //@import url('')
-let score = 0;
-let lista = [];
-let intervalId;
-let reloj = 6;
-let array = ['e','l','r','t','t','y','a','o','o','t','t','ñ','a','b','b','j','o','o','e','h','r','t','v','w','c','i','m','o','t','u','d','i','s','t','t','y','e','i','o','s','s','t','d','e','l','r','v','y','a','c','h','o','p','s','h','i','m','n','u','q','e','e','i','n','s','u','e','e','g','h','n','ñ','a','f','f','k','p','s','h','l','n','n','r','z','d','e','i','l','r','x']
-
-
-//------------------VARIALES GLOBALES
 const $start = document.querySelector("start");
 const $canvas = document.querySelector("canvas");
 const ctx = $canvas.getContext("2d");
 
+let score = 0;
+let lista = [];
+let intervalId;
+let cellSize = 150;
+let reloj = 6;
+let array = ['e','l','r','t','t','y','a','o','o','t','t','ñ','a','b','b','j','o','o','e','h','r','t','v','e','c','i','m','o','t','u','d','i','s','t','t','y','e','i','o','s','s','t','d','e','l','r','v','y','a','c','h','o','p','s','h','i','m','n','u','q','e','e','i','n','s','u','e','e','g','h','n','ñ','a','f','f','k','p','s','h','l','n','n','r','z','d','e','i','l','r','x']
 
-//------------------CLASES
+const dadosArray = []; //Un arreglo que contiene todos los dados que se van da dibujar
+const gameGrid = []; // Un arreglo que contiene a todos los cuadros donde están los dados
+
+const palabraArray = []; //un arreglo donde se van a ir empujando las letras donde se hace click
+
+const cellGap = 5; //esto es lo que evitaría la colisión, y así permitir movimientos diagonales
+const mouse = { //Mouse (pos) posiciona el mouse adentro del grid
+    x: 10,
+    y: 10,
+    width: 0,
+    height: 0,
+}
+
+
+// getBoundingClientRect -> esta funcion regresa un objecto del DOM (rectangulo) que contiene info del tamaño de algun elemento y su posicion
+// sirve para saber donde se puede mover el mouse dentro del CANVAS y no fuera
+let canvasPosition = $canvas.getBoundingClientRect();
+//El método addEventlistener, es un escuchador que indica al navegador que este atento a la interacción del usuario.
+//Tipo de evento: MouseEvent | UIEvent | Event | ProgressEvent<EventTarget> | ClipboardEvent | AnimationEvent | WheelEvent. ejemplos: mousedown, mouseenter, mouseleave, mousemove, *click*
+
+$canvas.addEventListener('mousemove', function(event){
+    mouse.x = event.x - canvasPosition.left; // el mouse accede a las coordinadas del canvas global
+    mouse.y = event.y - canvasPosition.top;
+})
+
+$canvas.addEventListener('mouseleave', function(event){ //cuando el mouse sale del canvas: mouseleave ocurre cuando el puntero del mouse se mueve fuera de un elemento.
+    mouse.y = undefined;                                // regresa a la posicion acual del mouse obj.
+    mouse.x = undefined; 
+})
+
+
+//------------------CLASES-----------------------
+
+//handlecreategrid y handlegamegrid es en donde dibuja todo
+
+//--------------------CELDAS---------------------
+class Cell {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+
+    draw(){
+        //si mouse.x tiene cordenadas (verdadero) y mouse.y tiene coordenas (verdadero) y...
+        // si (defensor = this.cell (esta instancia) y la posicion del mouse) - regresa o pinta esta misma cell(instancia)
+        if (mouse.x && mouse.y) {
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);  
+        }
+    }
+}
+
+function createGrid() {
+    // El loop entra al canvas por rows en este loop, primero empieza debajo de la linea azul (100px en hight) y 
+    //va iterando 100px en X hasta terminar el $canvas.width y baja de nuevo en 100px (cellSeize)
+    for (let y = 0; y < $canvas.height; y += cellSize) {
+        for (let x = 0; x < $canvas.width; x += cellSize) {
+            // cada vez que nos movemos horizontalmente en X en 100px (cellSize) se manda al array un nuevo Cell
+            //se ponen Cell por todo el canvas en X y Y
+            gameGrid.push(new Cell(x,y));
+        }
+    }
+}
+
+function cellGrids() {     // Loop que itera en el array global que se va a ir llenando en mi event click de Crear nuevo dogeKiller
+    for (let i = 0; i < gameGrid.length; i++) {
+        gameGrid[i].draw();
+    }
+}
+
 class Board {
     constructor() {
         this.x = 0;
@@ -49,11 +118,34 @@ class Dado {
     }
 }
 
+
+
+isTouching(obj) {
+    return (
+        this.x < obj.x + obj.width &&
+        this.x + this.width > obj.x &&
+        this.y < obj.y + obj.height &&
+        this.y + this.height > obj.y
+    );
+}
+
    // newImage(element,imageurl) {
      //       element.style.backgroundImage = "url("+imageurl+")";
        //  }
 }
 
+
+
+
+/*class MouseClick { //dogekiller
+    constructor(x,y){
+        this.x = x;
+        this.y = y; 
+        this.width = cellSize - cellGap * 2; // <- reduce el cuadro
+        this.hight = cellSize - cellGap * 2;
+    }
+        
+}*/
 
 //-------------------------INSTANCIAS
 let board = new Board();
@@ -77,24 +169,18 @@ let dado16 = new Dado(450, 450, xLetra(4), yLetra(4), `${revolver(array).toUpper
 
 function revolver(array) {
     let random = Math.floor(Math.random() * array.length);
-    return array[random];
-    //esto regresa un número del 0 al 5, que se va a usar para elegir una letra de cada dado
-    //sin embargo, necesito asignar a cada una de estas letras una respectiva imagen
+    return array[random];     //esto regresa un número de array, que se va a usar para elegir una letra de cada dado
     }
 
-function xLetra(num) {
+function xLetra(num) { //ubica las letras adentro del cuadro en x
     return (600/4)*num-(600/8)-18;
 }
 
-function yLetra(num) {
+function yLetra(num) { //ubica las letras adentro del cuadro en y
     return (600/4)*num-(600/8)+18
 }
 
-function revolverDados() {
-
-//esto debería realizar la función revolver para todos los dados
-//sin embargo, es repetitivo, y debería poder usar la clase dado para crearlo
-//también debería ser intuitivo el usar la imagen que tenga asignada
+function revolverDados() {//esto debería realizar la función revolver para todos los dados
 }
 
 function mezclarDados(dados) {
@@ -104,29 +190,21 @@ function mezclarDados(dados) {
     //¿Pero no sería mejor en este caso usar todas las letras en cada dado? pero así las vocales salen muy poquito
 }
 
-function armarPalabra(click) {
-    //esto debe juntar la elección de cada letra en orden
+function armarPalabra(click) {//esto debe juntar la elección de cada letra en orden
     palabra.push(click);
 }
 
-function revisarPalabra(palabra) {
-    forEach((palabra.split("").join("")) = (letra) => {
-        
+function revisarPalabra(palabra) {//esto debe revisar si cada letra está concatenada a la anterior Y si no se usó el mismo dado para la palabra
+    forEach((palabra.split("").join("")) = (letra) => { //mi problema es que no quiero la palabra misma ni sus letras, sino los botones que se usaron
     });
-    //esto debe revisar si cada letra está concatenada a la anterior Y si no se usó el mismo dado para la palabra
-    //mi problema es que no quiero la palabra misma ni sus letras, sino los botones que se usaron
-    }
+        }
     
 
-function listaPalabra(palabra) {
-    //esto debe revisar si la palabra está en el diccionario Y si no se escribió ya anteriormente. 
+function listaPalabra(palabra) { //esto debe revisar si la palabra está en el diccionario Y si no se escribió ya anteriormente. 
  return palabra
 }
 
-function addPalabra(palabra) {
-    //¿qué pasa con las ñ en una función? seguro trae problemas en algunos navegadores
-    //esto debe ser un botón que invoca revisarPalabra y listaPalabra
-    //Si pasa el checklist, entonces sumarla a una lista nueva 
+function addPalabra(palabra) { //esto debe ser un botón que invoca revisarPalabra y listaPalabra. Si pasa el checklist, entonces sumarla a una lista nueva 
     return revisarPalabra(palabra) && listaPalabra(palabra) ? lista.push(palabra) : null;
 }
 
@@ -158,18 +236,16 @@ dado13.draw();
 dado14.draw();
 dado15.draw();
 dado16.draw();
+cellGrids();
+//checkCollitions();
 
-
-  //checkCollitions();
 }
-
 
 function update() {
     reloj--;
     console.log(reloj);
     if (reloj === 0) endGame();
     board.draw();
-
 }
 
 function endGame() {
@@ -185,6 +261,7 @@ function endGame() {
 
 function reStart() {
 //esto debería permitir que se pueda volver a hacer click en start
+board.draw();
 }
 
 window.onload = (event) => {
@@ -198,47 +275,15 @@ document.getElementById('start').onclick = () => {
 }
 
 
+$canvas.addEventListener('click', function(){
+    // Tomaremos la coordenada principal o original del mouse en X y Y
+    // supongamos que la posicion del mouse es 250 en X y cellSize = 150 entonces 250 - (150) = 100 
+    // Esto es el valor de la posicion de mi Celda en X a la izquierda (son 4 columnas - 600px)
+    const gridPositionX = mouse.x - (mouse.x % cellSize) + cellGap;
+    const gridPositionY = mouse.y - (mouse.y % cellSize) + cellGap;
+        for (let i = 0; i < dogeKillers.length; i++) {
+            if (dogeKillers[i].x === gridPositionX && dogeKillers[i].y === gridPositionY) {
+                return; // <---- Si la posicion del defensor que ya habia colocado es igual al click de mi nueva CELDA (NO HAGAS NADA)
+            }}
 
-
-
-
-
-
-
-/*const $diccionario = require('diccionario');
-  
-diccionario.readFile('diccionario.txt', (err, data) => {
-    if (err) throw err;
-  
-    console.log(data.toString());
-// esto me servirá tal vez leer el diccionario
-
-})*/
-
-
-/*
-const removeAccents = (str) => {
-  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-} 
-
-esto me permite quitar acentos
-*/
-
-
-
-/*
-
-*/
-
-
-/*const dados = [[dado1, dado2, dado3, dado4], // AAEEGN, ELRTTY, AOOTTW, ABBJOO, 
-[dado5,dado6, dado7, dado8], // EHRTVW, CIMOTU, DISTTY, EIOSST, 
-[dado9, dado10, dado11, dado12], // DELRVY, ACHOPS, HIMNQuU, EEINSU, 
-[dado13, dado14, dado15, dado16, ]]; // EEGHNW, AFFKPS, HLNNRZ, DEILRX*/
-
-
-//no sé si sea mejor en objeto, en array de arrays, o una clase, 
-//pero sí es importante que no sean completamente aleatorios, para que haya más vocales que consonantes
-//me preocupa que se repitan tanto estas variables. no me parece óptimo
-
-
+})
