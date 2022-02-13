fe75428642dcf232b6f7be8e31dbb664c8268d84
pablo valdés <pablovvb@gmail.com>
2022-02-12 23:24:05 -0600

    solved the counter issue, the game works now. The CSS is next to go

--- functions/countScore.js
+++ functions/countScore.js
@@ -1,16 +1,13 @@
-export default function countScore(palabra, score) { //esto cuenta el largo de cada palabra del array lista, y suma puntos al score.
+// export default function countScore(palabra) { //esto cuenta el largo de cada palabra del array lista, y suma puntos al score.
     
-      let length = palabra.length;
-      if(length === 3 || length === 4) {
-          score += 1;
-      } else if (length === 5) {
-          score += 2;
-      } else if (length === 6) {
-          score += 3
-      } else if (length === 7) {
-          score += 5;
-      } else if (length > 7) {
-          score += 10
-      }
-    return score;
-  }
+//       const length = palabra.length;
+
+//       if        (length === 3)  {score ++
+//       } else if (length === 4)  {score ++
+//       } else if (length === 5)  {score += 2
+//       } else if (length === 6)  {score += 3
+//       } else if (length === 7)  {score += 5
+//       } else if (length  >  7)  {score += 10
+//       }
+//       console.log(score)
+//   }
